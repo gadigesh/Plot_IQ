776cf36306a6f94689f0ce7a0841f888ae60d97a
Gadigesh Hiremath <gchiremath.23@gmail.com>
2025-07-04 23:48:49 +0530

    updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () => {
   return (
     <div className="w-screen p-2 shadow-md flex items-center justify-center bg-gray-200 -inset-4top-0.5">
-      <div className="m-2 p-2 flex font-bold text-2xl ">
+      <div className="m-2 p-2 flex font-bold text-xl md:text-4xl">
         Floor Plan & Construction Estimator
       </div>
     </div>
